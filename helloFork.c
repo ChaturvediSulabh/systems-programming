08568d212c994b56d2c4ffd78088b2abb3a333d0
Sulabh Chaturvedi <>
2020-10-19 11:29:26 +0100

    Concurrency play without wait()

---
+++ helloFork.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/*
+Write another program using fork(). The child process should print “hello”; the parent process should print “goodbye”. You should try to ensure that the child process always prints first; can you do this without calling wait() in the parent?
+*/
+int main(){
+    int rc = fork();
+    if (rc < 0){
+        printf("fork failed");
+    } else if (rc == 0){
+        printf("Hello\n");
+        // child
+    } else {
+        sleep(1);
+        printf("GoodBye!\n");
+        //parent
+    }
+}
